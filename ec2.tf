4c75ba2deda554aac507e7e2c03fa800a3f1fcee
Khanh <khan.hadi2951@gmail.com>
2024-09-30 09:32:54 +0500

    added ec2 config

---
+++ ec2.tf
@@ -0,0 +1,65 @@
+resource "tls_private_key" "db-private-key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "db-key-pair" {
+  key_name   = "backend-dhoondlai-key-pair"
+  public_key = tls_private_key.backend-dhoondlai-private-key.public_key_openssh
+}
+
+data "aws_ami" "mongodb-ubuntu" {
+  owners = ["self"]
+  filter {
+    name   = "name"
+    values = ["mongodb-ubuntu-22"]
+  }
+}
+
+resource "aws_instance" "db-instance" {
+  ami           = data.aws_ami.mongodb-ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "backend-dhoondlai-key-pair"
+
+  subnet_id                   = aws_subnet.main-public-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.main-vpc-web-sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+  apt-get update -y
+  apt-get install -y nginx
+  EOF
+
+  iam_instance_profile = aws_iam_instance_profile.ec2-iam-instance-profile.name
+
+  tags = {
+    Name = "backend-parhlai-instance"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2-iam-instance-profile" {
+  name = "ec2-iam-instance-profile"
+  role = aws_iam_role.ec2-iam-role.name
+}
+
+resource "aws_iam_role" "ec2-iam-role" {
+  name = "ec2-iam-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-iam-role-policy-attachment" {
+  role       = aws_iam_role.ec2-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
--- main.tf
+++ main.tf
@@ -1,20 +1,52 @@
-# dynamodb table. Core database for our application.
+resource "aws_vpc" "main-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
 
-module "dynamodb_table" {
-  source = "terraform-aws-modules/dynamodb-table/aws"
+# Subnets
 
-  name      = "products"
-  hash_key  = "name"
-  range_key = "vendor"
+resource "aws_subnet" "main-public-subnet-1" {
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
 
-  attributes = [
-    {
-      name = "name"
-      type = "S"
-    },
-    {
-      name = "vendor"
-      type = "S"
-    }
-  ]
+resource "aws_subnet" "main-private-subnet-1" {
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+}
+
+# Internet gateway
+
+resource "aws_internet_gateway" "main-igw" {
+  vpc_id = aws_vpc.main-vpc.id
+}
+
+
+# Route table
+
+resource "aws_route_table" "main-public-rt" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.main-igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.main-public-subnet-1.id
+  route_table_id = aws_route_table.main-public-rt.id
+}
+
+# Security group
+
+resource "aws_security_group" "main-vpc-web-sg" {
+  name   = "main-vpc-web-sg"
+  vpc_id = aws_vpc.main-vpc.id
 }
