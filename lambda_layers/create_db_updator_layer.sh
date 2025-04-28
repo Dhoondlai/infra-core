004388b4983683d06b1b7bb278d7ed7a6df57779
okHadi <khan.hadi2951@gmail.com>
2025-04-29 00:20:15 +0500

    fix: update db updator layer pathing, function description

--- lambda_layers/create_db_updator_layer.sh
+++ lambda_layers/create_db_updator_layer.sh
@@ -12,10 +12,10 @@ pip install pymongo
 pip install pymongo-auth-aws
 
 # Create the python directory
-mkdir python
+mkdir -p python/lib/python3.12/site-packages
 
-# Copy the necessary library files to your python directory
-cp -r create_db_updator_layer/lib/python* python/
+# Copy the necessary library files to the correct directory structure for Lambda layers
+cp -r create_db_updator_layer/lib/python3.12/site-packages/* python/lib/python3.12/site-packages/
 
 # Zip the contents of the python directory
 zip -9 -q -r db_updator_layer_content.zip python
